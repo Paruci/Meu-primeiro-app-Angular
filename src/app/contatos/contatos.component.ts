62d08ada71f8bea57f22797c0aad3415bc3eec96
Paruci <ppmlvd@gmail.com>
2026-04-28 16:35:36 -0300

    Novo espaco para cadastro

--- src/app/contatos/contatos.component.ts
+++ src/app/contatos/contatos.component.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contatos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contatos.component.html',
   styleUrl: './contatos.component.css'
 })
 export class ContatosComponent {
-  
+
+  novoContato = { nome: '', telefone: '', email: '' };
+
   contatos = [
     { id: 1, nome: 'João Silva', telefone: '(11) 99999-1111', email: 'joao@email.com' },
     { id: 2, nome: 'Maria Souza', telefone: '(11) 99999-2222', email: 'maria@email.com' },
   ];
+
+  adicionarContato() {
+    if (!this.novoContato.nome) return;
+
+    this.contatos.push({
+      id: this.contatos.length + 1,
+      ...this.novoContato
+    });
+
+    this.novoContato = { nome: '', telefone: '', email: '' };
+  }
 
 }
