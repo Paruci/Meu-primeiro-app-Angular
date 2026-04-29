749501a1f50849a6de3110a2aaeea33c3f0d6c1d
Paruci <ppmlvd@gmail.com>
2026-04-29 13:17:34 -0300

    Ajuste em rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { SobreComponent } from './sobre/sobre.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'sobre', component: SobreComponent },
+  { path: 'home', component: Home },
+  { path: 'sobre', component: Sobre },
+  { path: 'contatos', component: ContatosComponent },
 ];
